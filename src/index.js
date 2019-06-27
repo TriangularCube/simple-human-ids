ad7376ac0ec8bbba8ecf960829121d860dbce620
Michael Liu <michael.liu0@gmail.com>
2019-06-26 23:50:16 -0700

    Nightly June 26

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const shuffle = require( 'shuffle-array' );
+
+function random( lists = construct() ){
+
+
+
+}
+
+function construct( number, animals, adjectives ){
+
+}
+
+function getList( subject ){
+
+}
+
+module.exports = random;
+module.exports.constructLists = construct;
---
+++ tests/test.js
@@ -0,0 +1,4 @@
+const id = require( '../src/index' );
+const lists = id.constructLists();
+
+console.log( id() );
