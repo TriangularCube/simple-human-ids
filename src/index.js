88cf3e68fb22c3cd73f16d12ce72ea2900c0a596
Michael Liu <michael.liu0@gmail.com>
2019-06-27 14:41:25 -0700

    Now working package

--- src/index.js
+++ src/index.js
@@ -1,18 +1,69 @@
 const shuffle = require( 'shuffle-array' );
 
-function random( lists = construct() ){
+let lists;
 
+function configure( newList ){
+    // TODO maybe do some validation
+    lists = newList;
+}
 
+function next(){
+
+    // Initialize lists if not already done
+    if( !lists ){
+        lists = construct( generateNumbers(), getList( 'animals' ), getList( 'adjectives' ) );
+    }
+
+    // Start a new string
+    return `${shuffle.pick( lists.numbers )}-${shuffle.pick( lists.adjectives)}-${shuffle.pick( lists.animals )}`;
 
 }
 
-function construct( number, animals, adjectives ){
+function construct( numbers, animals, adjectives ){
+
+    let lists = {};
+    lists['numbers'] = numbers;
+    lists['animals'] = animals;
+    lists['adjectives'] = adjectives;
+
+    return lists;
 
 }
 
+function generateNumbers(){
+
+    let numbers = [];
+    numbers.push( 0 );
+
+    // 1 is not plural, so we skip
+
+    for( let i = 2; i < 100; i++ ){
+        numbers.push( i );
+    }
+
+    return numbers;
+
+}
+
+let fs;
+let path;
 function getList( subject ){
 
+    // Import filesystem and path utilities if not already imported
+    if( !fs ){
+        fs = require( 'fs' );
+    }
+    if( !path ){
+        path = require( 'path' );
+    }
+
+    const filePath = path.join( __dirname, '..', 'data', subject + '.txt' );
+    const file = fs.readFileSync( filePath, 'utf8' );
+
+    return file.trim().split( '\n' );
+
 }
 
-module.exports = random;
+module.exports.new = next;
+module.exports.configure = configure;
 module.exports.constructLists = construct;
--- tests/test.js
+++
@@ -1,4 +0,0 @@
-const id = require( '../src/index' );
-const lists = id.constructLists();
-
-console.log( id() );
